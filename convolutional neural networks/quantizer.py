4d2e3de4f905acb0d44b6d683c85a587c3866cd8
Preet Kanwal <s.preetkanwal@gmail.com>
2018-09-04 21:36:41 +0530

    modification - quantizer code
    modification - quantizer code

--- convolutional neural networks/quantizer.py
+++ convolutional neural networks/quantizer.py
@@ -29,4 +29,23 @@ class1 = data[labels[:,0] == 1]
 class2 = data[labels[:,1] == 1]
 class3 = data[labels[:,2] == 1]
 class4 = data[labels[:,3] == 1]
-class4 = data[labels[:,3] == 1]
+
+# Define grids for all the 4 classes
+grid1 = input_grid[output_grid[:,0] == 1]
+grid2 = input_grid[output_grid[:,1] == 1]
+grid3 = input_grid[output_grid[:,2] == 1]
+grid4 = input_grid[output_grid[:,3] == 1]
+
+# Plot outputs 
+plt.plot(class1[:,0], class1[:,1], 'ko', class2[:,0], class2[:,1], 'ko', 
+                class3[:,0], class3[:,1], 'ko', class4[:,0], class4[:,1], 'ko')
+plt.plot(grid1[:,0], grid1[:,1], 'b.', grid2[:,0], grid2[:,1], 'gx',
+                grid3[:,0], grid3[:,1], 'cs', grid4[:,0], grid4[:,1], 'ro')
+plt.axis([0, 8, 0, 8])
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Vector quantization using neural networks')
+
+plt.show()
+
+
