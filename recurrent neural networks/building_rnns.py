a27093f9dc45752616fd466670472ee4dd8cfc3a
Preet Kanwal <s.preetkanwal@gmail.com>
2018-09-12 21:10:22 +0530

    rnns continued - forward_cell
    forward cell rnns

--- recurrent neural networks/building_rnns.py
+++ recurrent neural networks/building_rnns.py
@@ -1,2 +1,15 @@
 import numpy as np
 from rnn_utils import *
+
+
+def rnn_cell_forward(xt, a_prev, parameters):
+
+	Wax = parameters["Wax"]
+    Waa = parameters["Waa"]
+    Wya = parameters["Wya"]
+    ba = parameters["ba"]
+    by = parameters["by"]
+	
+	a_next = np.tanh(np.dot(Wax, xt) + np.dot(Waa, a_prev) + ba)
+    # compute output of the current cell using the formula given above
+    yt_pred = softmax(np.dot(Wya, a_next) + by)
